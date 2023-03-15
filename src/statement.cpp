ad6778983db80ad1e051f4490db6ff59c1217c4b
ItsFreSsh <lukes.fr@seznam.cz>
2023-03-15 10:47:13 +0100

    Added statement name to statement print

--- src/statement.cpp
+++ src/statement.cpp
@@ -3,6 +3,7 @@
 
 void Statement::print() const{
     std::cout << "\t\t\t\t\t" << "\"stmt\": {" << std::endl;
+    std::cout << "\t\t\t\t\t\t" << "\"name\": \"" << name << "\"" << std::endl; 
     std::cout << "\t\t\t\t\t}," << std::endl;
     loc.print();
 }
