319c511c708267ac5f5f6cf7d579c4084d687963
ItsFreSsh <lukes.fr@seznam.cz>
2023-03-19 11:55:03 +0100

    Updatedprocess  function's parameter loop

--- src/process-function.cpp
+++ src/process-function.cpp
@@ -14,25 +14,22 @@
 #include "function.h"
 #include "process-function.h"
 #include "process-block.h"
+#include "process-function-parameters.h"
+#include "data-type.h"
 
-Function processFunction(function *func) {
+Function processFunction(function* func) {
     Function f;
     f.name = function_name(func);
 
     // Entry parameters
-    /*tree param;
+    tree param;
     for (param = DECL_ARGUMENTS(func->decl); param; param = DECL_CHAIN(param)) {
-		//std::string param_type = get_parameter_type(TREE_TYPE(param));
-		param_to_add["name"] = get_name(param);
-		param_to_add["type"] = get_decl_type(TREE_TYPE(param));
-		//get_decl_type(TREE_TYPE(param));
-		output[function_name(fun)]["entry_parameters"].append(param_to_add);
-		++nparams;
-	}*/
+        f.insertParameter(processFunctionParameter(param));
+    }
 
     basic_block bb;
     FOR_ALL_BB_FN(bb, func) {
-        gimple_bb_info *bb_info = &bb->il.gimple;
+        gimple_bb_info* bb_info = &bb->il.gimple;
         // Entry block
         if (bb->index == 0) {
             f.entry_block = "bb " + std::to_string(bb->index);
