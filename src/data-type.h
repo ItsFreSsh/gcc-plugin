db6f9b904366586eca1d5dcbad6b9a905e2ab136
ItsFreSsh <lukes.fr@seznam.cz>
2023-03-19 11:52:43 +0100

    Adjusted some comments

--- src/data-type.h
+++ src/data-type.h
@@ -6,12 +6,12 @@
 class DataType {
 public:
     std::string name;
-    size_t sizeOf;
+    size_t sizeOf; // size in bytes
     size_t precision;
     long long int min;
     unsigned long long int max;
     bool signedness;
-    DataType* type; // pointer to another DataType object for pointed-to type
+    DataType* type; // pointer to another DataType object
     void print(unsigned int indentBy) const;
 };
 
